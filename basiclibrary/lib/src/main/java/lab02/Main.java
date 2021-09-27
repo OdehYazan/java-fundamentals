430f646da25e046e98c8d748c7bb1114bba68369
OdehYazan <eng.yazanodeh@gmail.com>
2021-09-27 23:03:38 +0300

    lab02

---
+++ basiclibrary/lib/src/main/java/lab02/Main.java
@@ -0,0 +1,25 @@
+package lab02;
+
+public class Main {
+    public static void main(String[] args) {
+       int [] arr ={1,2,3,4,5,6,6};
+        int[][] weeklyMonthTemperatures = {
+
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+
+                {55, 54, 60, 53, 59, 57, 61},
+
+                {65, 56, 55, 52, 55, 62, 57},
+
+        };
+
+        System.out.println("xxxxxxxxxxxxx");
+
+        Library fun = new Library();
+        fun.roll(2);
+        System.out.println(fun.containsDuplicates(arr));
+        System.out.println(fun.Average(arr));
+        System.out.println(fun.arraysLowestAav(weeklyMonthTemperatures));
+    }
+}
